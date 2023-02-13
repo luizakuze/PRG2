032bb055f3a4f6d0335ed7637d196137c47b0233
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-02-13 15:24:56 -0300

    Create Conta_palavras_e_Strings.cpp

---
+++ Introdução/Conta_palavras_e_Strings.cpp
@@ -0,0 +1,37 @@
+// Contador de palavras e caracteres
+// O programa conta o número de letras e de palavras de uma frase
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string texto;
+    bool espaco = true;
+    int qt_palavra = 0, qt_caract = 0;
+
+    getline(cin, texto);
+
+    for (int i = 0; i < texto.size(); i++) {
+
+        //quantidade de caracteres
+        qt_caract++;
+
+        //quantidade de palavras
+        if (texto[i] != ' ') {
+            if (espaco) {
+              qt_palavra++;
+              espaco = false;
+            }
+        } else {
+            espaco = true;
+        }
+        
+    }
+
+    cout << qt_palavra << ' ' << qt_caract << endl;
+
+    return 0;
+}
