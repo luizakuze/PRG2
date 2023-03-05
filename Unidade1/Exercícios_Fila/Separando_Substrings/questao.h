a80f0ed7f2e60c8bcb7e6aaf8298c0214b1848de
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-03-05 14:32:58 -0300

    Create questao.h

---
+++ Unidade1/Exercícios_Fila/Separando_Substrings/questao.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_CPP_QUESTAO_H
+#define MAIN_CPP_QUESTAO_H
+
+#include <string>
+#include <queue>
+
+using namespace std;
+
+queue<string> separa(const string & algo, char sep);
+
+#endif //MAIN_CPP_QUESTAO_H
