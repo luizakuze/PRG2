af505ed128f232b13d7decad4517c3c63d8954d5
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-03-07 22:45:33 -0300

    Create questao.h

---
+++ Unidade1/Exercícios_Pilha/Caminho_arquivo/Versão_Arquivos/questao.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_CPP_QUESTAO_H
+#define MAIN_CPP_QUESTAO_H
+
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string reduz_caminho(const string & caminho);
+
+#endif //MAIN_CPP_QUESTAO_H
