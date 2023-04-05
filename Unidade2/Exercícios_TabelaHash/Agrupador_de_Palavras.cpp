5d71e7c3960d353f5d6b5d47d0822d178cefe878
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-04-05 12:17:14 -0300

    Update Agrupador_de_Palavras.cpp

--- Unidade2/Exercícios_TabelaHash/Agrupador_de_Palavras.cpp
+++ Unidade2/Exercícios_TabelaHash/Agrupador_de_Palavras.cpp
@@ -15,9 +15,8 @@ bola
 ceu mar 
 caju pato bola
 banana
-
-As linhas devem estar ordenadas de acordo com o comprimento de palavras.
-As palavras de cada linha devem aparecer na ordem em que foram lidas do arquivo.
+As linhas devem estar ordenadas de acordo com o comprimento de palavras. 
+s palavras de cada linha devem aparecer na ordem em que foram lidas do arquivo.
 
 O nome do arquivo de palavras é fornecido como primeiro argumento de linha de comando.
 Se o arquivo não puder ser aberto, esta mensagem de erro deve ser apresentada:
@@ -28,55 +27,52 @@ Arquivo invalido
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 #include <list>
 #include <unordered_map>
 
 using namespace std;
 
-// FUnção que indica para o sort que ele deve ordenar pela quantidade de palavras
-bool compara_par (const pair<string,int> & p1, const pair<string,int> & p2) {
+// junta -> junta as strings e separa por espaços (função separa)
+string junta (vector <string> & v) {
+    string r; //resultado
 
-    // Se tiverem a mesma quantidade de caracteres
-    if(p1.second == p2.second) {
-        return p1.first < p2.first;
+    if (!v.empty()) {
+        for (auto &dado: v) {
+            r += dado + ' ';
+        }
+        r.pop_back();
     }
-    // Ordena decrescente
-    return p1.second > p2.second;
+    return r;
 }
-
 int main(int argc, char *argv[])
 {
     // Abre arquivo para leitura
     ifstream arq (argv[1]);
 
-    // Cria um conjunto com palavras e suas quantidades
-    unordered_map<string,int> totais;
-
-    // Adiciona cada dado do arquivo ao conjunto
-    // O conjunto contém somente dados únicos
-    string palavra;
-
-    // Conta as palavras
-    while (arq >> palavra) {
-            totais[palavra]++;
+    // Verifica se o arquivo foi realmente aberto
+    if (!arq.is_open()) {
+        cerr << "Arquivo invalido" << endl;
+        return 1;
     }
 
-    // Cria uma lista com palavras e número de repetições
-    list<pair<string,int>> resultado (totais.begin(), totais.end());
+    unordered_map<int, vector<string>> tab;
+    string palavra;
+    while (arq >> palavra) {
+        //vector<string> & v = tab[palavra.size()];
+        //v.push_back(palavra);
+        tab[palavra.size()].push_back(palavra);
+    }
 
-    // Como é a forma de comparar
-    resultado.sort(compara_par);
+    list <int> l_chaves;
+    for (auto & par: tab) {
+        l_chaves.push_back(par.first);
+    }
+    l_chaves.sort();
 
-    // Imprime dados
-    int cont = 0;
-    for (auto &dado: resultado) {
-        // Verifica se já imprimiu x vezes
-        if (cont == stoi(argv[2])) {
-            break;
-        } else {
-            cout << dado.first << ' ' << dado.second << endl;
-            cont++;
-        }
+    // Imprime palavras
+    for (auto & chave: l_chaves) {
+        cout << junta(tab[chave]) << endl;
     }
 
     return 0;
