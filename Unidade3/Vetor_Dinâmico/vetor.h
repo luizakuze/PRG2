0d9cd767bd4dddc774b9571775bc4c9f0e083188
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-05-25 15:52:35 -0300

    Update vetor.h

--- Unidade3/Vetor_Dinâmico/vetor.h
+++ Unidade3/Vetor_Dinâmico/vetor.h
@@ -10,14 +10,15 @@ namespace prg2 {
     const int MinSize = 8;
 
     // O tipo VetorDinamico
+    template <typename T>
     struct VetorDinamico {
         // como definir a área de memória de armazenamento ??
-        int * mem;
+        T* mem;
 
         uint32_t len; // quantos dados estão armazenados
         uint32_t capacidade; // qual a capacidade
 
-        // posição do primeiro dado armazenado e d aprimeira posição livre (FILA CIRCULAR)
+        // posição do primeiro dado armazenado e da primeira posição livre (FILA CIRCULAR)
         uint32_t inicio, fim;
     };
 
@@ -25,88 +26,115 @@ namespace prg2 {
 
     // cria um VetorDinamico com capacidade inicial dada por
     // MinSize
-    VetorDinamico vetor_cria();
+    template <typename T>
+    VetorDinamico<T> vetor_cria();
 
     // Destroi um vetor (destrutor)
-    void vetor_destroi(VetorDinamico &v);
+    template <typename T>
+    void vetor_destroi(VetorDinamico<T> &v);
 
     // Acrescenta um valor ao final do vetor
     // Expande o vetor, caso necessário. A expansão
     // aloca uma capacidade adicional dada por MinSize
-    void vetor_anexa(VetorDinamico &v, int valor);
+    template <typename T>
+    void vetor_anexa(VetorDinamico<T> &v, const T& valor);
 
     // Insere um valor no início do vetor
     // Expande o vetor, caso necessário. A expansão
     // aloca uma capacidade adicional dada por MinSize
-    void vetor_insere(VetorDinamico &v, int valor);
+    template <typename T>
+    void vetor_insere(VetorDinamico<T> &v, const T& valor);
 
     // Insere um valor na posição pos do vetor
     // Expande o vetor, caso necessário. A expansão
     // aloca uma capacidade adicional dada por MinSize
     // Dispara exceção se pos > tamanho do vetor
-    void vetor_insere(VetorDinamico &v, int pos, int valor);
+    template <typename T>
+    void vetor_insere(VetorDinamico<T> &v, int pos, const T& valor);
 
     // Acessa o dado da frente da vetor
     // se vetor vazia: o que acontece ????
-    // DISPARA EXCEÇÃO do tipo invalid_argument
-    int& vetor_frente(VetorDinamico & v);
+    // DISPARA EXCEÇÃO do tipo invalid_argument]
+    template <typename T>
+    T & vetor_frente(VetorDinamico<T> & v);
 
     // Acessa o dado do final do vetor
     // se vetor vazia: o que acontece ????
     // DISPARA EXCEÇÃO do tipo invalid_argument
-    int& vetor_atras(VetorDinamico & v);
+    template <typename T>
+    T & vetor_atras(VetorDinamico<T> & v);
 
     // remove o dado do início
     // se vetor vazia: DISPARA EXCEÇÃO do tipo invalid_argument
-    void vetor_remove_inicio(VetorDinamico & v);
+    template <typename T>
+    void vetor_remove_inicio(VetorDinamico<T> & v);
 
     // remove o dado do fim
     // se vetor vazia: DISPARA EXCEÇÃO do tipo invalid_argument
-    void vetor_remove_fim(VetorDinamico & v);
+    template <typename T>
+    void vetor_remove_fim(VetorDinamico<T> & v);
 
     // retorna true se vetor estiver vazio
-    bool vetor_vazio(const VetorDinamico & v);
+    template <typename T>
+    bool vetor_vazio(const VetorDinamico<T> & v);
 
     // Acessa um dado que está na posição pos do vetor
     // Obtém uma referência mutável ao dado
     // Se pos > tamanho-1, dispara exceção
-    int &vetor_obtem(VetorDinamico &v, int pos);
+    template <typename T>
+    T & vetor_obtem(VetorDinamico<T> &v, int pos);
 
     // Acessa um dado que está na posição pos do vetor
     // Obtém uma referência somente-leitura ao dado
     // Se pos > tamanho-1, dispara exceção
-//     const int &vetor_obtem(const VetorDinamico &v, int pos);
+    template <typename T>
+    const T & vetor_obtem(const VetorDinamico<T> &v, int pos);
 
     // Remove um dado da posição pos do vetor
     // Se pos > tamanho-1, dispara exceção
-    void vetor_remove(VetorDinamico &v, int pos);
+    template <typename T>
+    void vetor_remove(VetorDinamico<T> &v, int pos);
 
     // Obtem o tamanho do vetor
-    int vetor_tamanho(const VetorDinamico &v);
+    template <typename T>
+    int vetor_tamanho(const VetorDinamico<T> &v);
 
     // Obtem a capacidade do vetor (quantos dados cabem no total com a memória atualmente alocada)
-    int vetor_capacidade(const VetorDinamico & v);
+    template <typename T>
+    int vetor_capacidade(const VetorDinamico<T> & v);
 
     // Esvazia o vetor
-    void vetor_limpa(VetorDinamico &v);
+    template <typename T>
+    void vetor_limpa(VetorDinamico<T> &v);
 
     // Ordena o vetor
-    void vetor_ordena(VetorDinamico &v);
+    template <typename T>
+    void vetor_ordena(VetorDinamico<T> &v);
 
     // Inverte o vetor
-    void vetor_inverte(VetorDinamico &v);
+    template <typename T>
+    void vetor_inverte(VetorDinamico<T> &v);
 
     // Remove dados contíguos duplicados
-    void vetor_unicos(VetorDinamico &v);
+    template <typename T>
+    void vetor_unicos(VetorDinamico<T> &v);
 
     // Expande a capacidade do vetor
     // A nova capacidade será >= capacidade solicitada
     // capacidade deve ser maior que capacidade atual, senão dispara
     // exceção
-    void vetor_expande(VetorDinamico &v, int capacidade);
+    template <typename T>
+    void vetor_expande(VetorDinamico<T> &v, int capacidade);
 
     // Contrai o vetor para caberem exatamente os dados atuais
-    void vetor_contrai(VetorDinamico &v);
+    template <typename T>
+    void vetor_contrai(VetorDinamico<T> &v);
 
+    // Mostra o vetor
+    template <typename T>
+    void mostra_vetor(VetorDinamico<T> & v);
 }
+
+#include "vetor-impl.h" // para dividir os arquivos .h
+
 #endif //DYNARRAY_VETOR_H
