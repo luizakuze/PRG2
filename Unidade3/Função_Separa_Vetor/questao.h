57dfc427b542a8d0fb3e388b027c47d7774cc47f
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-05-29 22:32:49 -0300

    Create questao.h

---
+++ Unidade3/Função_Separa_Vetor/questao.h
@@ -0,0 +1,13 @@
+#ifndef PROG_QUESTAO_H
+#define PROG_QUESTAO_H
+
+
+#include "vetor.h"
+
+using namespace prg2;
+using std::string;
+
+VetorDinamico<string> separa(const string & algo, char sep);
+
+
+#endif //PROG_QUESTAO_H
